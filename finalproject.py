c9f87ad60cdb0e19cf8993abd34ed43c706334c3
2017760 <2017760@stjean.org>
2016-08-09 15:11:38 -0400

    added keyboard command

--- finalproject.py
+++ finalproject.py
@@ -30,14 +30,33 @@ screen = pygame.display.set_mode(WINDOW_SIZE)
  
 # Set title of screen
 pygame.display.set_caption("Women")
-class Player():
-    def __init__(self, coins):
-        self.coins=coins
+
+# class Player():
+#     #def __init__(self, coins):
+#      #   self.coins=coins
+
+#     # Constructor function
+#     def __init__(self, color, x, y):
+#         # Make our top-left corner the passed-in location.
+#         self.sprite = self.circle
+#         self.circle.x = 670
+#         self.circle.y = 470
+
+#     def reset_player(self):
+#         self.circle.x = 670
+#         self.circle.y = 470
+
+#     # Find a new position for the player
+#     def update(self):
+#         self.circle.x += self.change_y
+#         self.circle.y += self.change_x
+
         
 
-    def move(self, speed):
-        #moves with each arrow key
-        self.x_point = self.x_point + speed
+#     def move(self, x_point):
+#         #moves with each arrow key
+#         self.x_point = self.x_point + speed
+        
  
 # Loop until the user clicks the close button.
 done = False
@@ -52,12 +71,20 @@ while not done:
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             done = True  # Flag that we are done so we exit this loop
-        if event.type == pygame.KEYDOWN:
-            if event.type == pygame.K_LEFT:
-                x_pos = x_pos-5
-            elif event.type == pygame.K_RIGHT:
-                x_pos = x_pos + 5
-        
+    
+    # for event in pygame.event.get():
+    #     print (event.type)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                y_pos=y_pos- HEIGHT
+            elif event.key == pygame.K_DOWN:
+                y_pos=y_pos+HEIGHT
+            elif event.key == pygame.K_LEFT:
+                x_pos=x_pos- WIDTH
+            elif event.key == pygame.K_RIGHT:
+                x_pos = x_pos+WIDTH 
+         
+            # Something similar for the up & down keys
     # Set the screen background
     screen.fill(BLACK)
  
@@ -74,7 +101,7 @@ while not done:
  
     # Limit to 60 frames per second
     clock.tick(60)
-    pygame.draw.circle(screen, BLUE, (x_pos,y_pos), 30)
+    circle = pygame.draw.circle(screen, BLUE, (x_pos,y_pos), 30)
     # Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
  
