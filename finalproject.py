9f98e9ff0d48d562663d71d5a3dc047714114f9b
076212000 <Sab_KP@yahoo.com>
2016-08-10 14:47:57 -0400

    collision

--- finalproject.py
+++ finalproject.py
@@ -30,18 +30,25 @@ screen = pygame.display.set_mode(WINDOW_SIZE)
  
 # Set title of screen
 pygame.display.set_caption("Women")
-
-class Player():
+width = 50
+height = 50
+class Player(pygame.sprite.Sprite):
    
 
     # Constructor function
+
     def __init__(self, x_point, y_point):
         # Make our top-left corner the passed-in location.
+        pygame.sprite.Sprite.__init__(self)
+
         self.x_point=x_point
         self.y_point=y_point
+        self.image=pygame.Surface([width,height])
+        self.image.fill(BLUE)
+        self.rect=self.image.get_rect()
 
     def draw(self):
-        pygame.draw.circle(screen, BLUE, (self.x_point,self.y_point), 30)
+        pygame.draw.circle(screen,BLUE,(self.x_point,self.y_point),20)
     
 
     def moveup(self) :
@@ -59,8 +66,8 @@ class Player():
 
 
     def reset_player(self):
-        self.circle.x = 670
-        self.circle.y = 470
+        self.circle.x = 680
+        self.circle.y = 480
 
     # Find a new position for the player
     def update(self):
@@ -94,24 +101,31 @@ class Coins():
             
 
 
-class Obstacle():
+class Obstacle(pygame.sprite.Sprite):
    
     
     def __init__(self, x_point, y_point, width, height, color):
+        super().__init__()
         self.x_point=x_point
         self.y_point=y_point
         self.width=width
         self.height=height
         self.color=color
+
     def draw(self):
            
        
         pygame.draw.rect(screen, self.color, [self.x_point, self.y_point, self.width, self.height])
-<<<<<<< HEAD
+# <<<<<<< HEAD
 player1=Player(670,470)
-=======
->>>>>>> 77d5fb4ce766851c53aa100af8168880c2327e71
+# =======
+# >>>>>>> 77d5fb4ce766851c53aa100af8168880c2327e71
 
+# collision = pygame.sprite.collide_rect(Player,Obstacle):
+
+# if pygame.sprite.collide_rect 
+
+#print(pygame.sprite.collide_rect(Player,Obstacle))
  
 wall1=Obstacle(595, 380, 106, 60,BLACK)
 wall2=Obstacle(488, 380, 106, 60,BLACK)
@@ -167,10 +181,10 @@ while not done:
                               (MARGIN + HEIGHT) * row + MARGIN,
                               WIDTH,
                               HEIGHT])
-<<<<<<< HEAD
+# <<<<<<< HEAD
     player1.draw()
    
-=======
+# =======
 
     wall1.draw()   
     wall2.draw()
@@ -180,7 +194,7 @@ while not done:
     wall6.draw()
     wall7.draw()
     wall8.draw()
->>>>>>> 77d5fb4ce766851c53aa100af8168880c2327e71
+# >>>>>>> 77d5fb4ce766851c53aa100af8168880c2327e71
 
     wall9.draw()
     wall10.draw()
